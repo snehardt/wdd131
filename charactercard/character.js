3956b531419c192b9160fe3d506cd42ab4f31a88
snehardt <spencer.ehardt@gmail.com>
2026-02-24 17:37:19 -0700

    added meta tags/ clean up

--- charactercard/character.js
+++ charactercard/character.js
@@ -1,32 +1,44 @@
-console.log("Character card works now");
-
 const character = {
+    name: "Snortleblat",
     class: "Swamp Beast Diplomat",
     level: 5,
-    health: 100
+    health: 100,
+
+    attacked: function() {
+        this.health -= 20;
+
+        if (this.health <= 0){
+            this.health = 0;
+            updateStats();
+
+            setTimeout(function() {
+            alert("Character dead");
+            }, 500);
+
+            return;
+        }
+        updateStats();
+    },
+
+    levelUp: function () {
+        this.level += 1;
+        updateStats();
+    }
 }
 
-document.querySelector('#charClass').textContent = character.class
-document.querySelector('#charLevel').textContent = character.level
-document.querySelector('#charHealth').textContent = character.health
+function updateStats() {
+    document.querySelector('.name').textContent = character.name
+    document.querySelector('#charClass').textContent = character.class
+    document.querySelector('#charLevel').textContent = character.level
+    document.querySelector('#charHealth').textContent = character.health
+}
 
 document.querySelector('#attackBtn').addEventListener("click", function() {
-    character.health -= 20;
-
-    if (character.health <= 0){
-        character.health = 0
-        document.querySelector("#charHealth").textContent = character.health;
-        setTimeout(function() {
-            alert("Character dead");
-        }, 250);
-    }
-
-    document.querySelector("#charHealth").textContent = character.health;
-    
+    character.attacked();
 })
 
 document.querySelector('#levelUpBtn').addEventListener("click", function() {
-    character.level += 1;
-
-    document.querySelector("#charLevel").textContent = character.level;
+    character.levelUp();
 })
+
+updateStats();
